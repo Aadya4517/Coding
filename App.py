b089f96851ba087d7d75cd9aafbc3398fc955b37
Aadya4517 <154816454+Aadya4517@users.noreply.github.com>
2023-12-25 19:31:27 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+name= st. text_input("Enter your Name")
+age= st. text_input("Enter your age")
+email= st. text_input("Enter your gmail")
+password= st. text_input("Enter your password")
+button= st.button("Submit")
+
+
